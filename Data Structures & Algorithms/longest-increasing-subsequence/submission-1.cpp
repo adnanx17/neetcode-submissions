4fd93d500a5f186423d40e45542e7f9ecb6288ce
adnanx17 <adnanspy1244@gmail.com>
2026-09-04 23:40:10 +0530

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int>lis(nums.size(),1);
+        int result=1;
+        for(int i=1; i<nums.size(); i++){
+           for(int j=0; j<i; j++){
+              if(nums[i]>nums[j]){
+                lis[i]=max(lis[i],lis[j]+1);
+              }
+           }
+
+           result=max(result,lis[i]);
+        }
+
+        return result;
+    }
+};
